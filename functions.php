940d876388ff8b41d76bdf96b334f153db58c103
marcelo <marcelo@contassconsultoria.com.br>
2022-08-15 17:21:51 -0300

    customizando consulta

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+function alura_intercambios_registrando_taxonomia()
+{
+    register_taxonomy('paises', 'destinos', array(
+        'labels' => ['name' => 'Países'],
+        'hierarchical' => true,
+    ));
+}
+
+add_action('init', 'alura_intercambios_registrando_taxonomia');
+
+function alura_intercambios_registrando_post_customizado()
+{
+    register_post_type(
+        'destinos',
+        [
+            'labels' => [
+                'name' => 'Destinos',
+            ],
+            'public' => true,
+            'menu_position' => 0,
+            'supports' => ['title', 'editor', 'thumbnail'],
+            'menu_icon' => 'dashicons-admin-site'
+        ]
+    );
+}
+
+add_action('init', 'alura_intercambios_registrando_post_customizado');
+
+function alura_intercambios_adicionando_recursos_ao_tema()
+{
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'alura_intercambios_adicionando_recursos_ao_tema');
+
+function alura_intercambios_registrando_menu()
+{
+    register_nav_menu(
+        'menu-navegacao',
+        'Menu navegação'
+    );
+}
+
+add_action('init', 'alura_intercambios_registrando_menu');
